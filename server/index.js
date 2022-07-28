5b9c2465d5431ba0e65bcf27d00548409f9423d6
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-07-28 12:03:13 +0530

    c

--- server/index.js
+++ server/index.js
@@ -13,15 +13,27 @@ app.use(cors());
 app.use(express.json());
 
 
-// mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URL,{
 
+})
+
+
+
+
+
+
+// const server=app.listen(process.env.PORT,()=>{
+//     console.log("Server started")
 // })
 
+const PORT=process.env.PORT || 5000;
 
-
-
-
-
-const server=app.listen(process.env.PORT,()=>{
-    console.log("Server started")
-})
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+  } catch {
+    console.log("failed connect to db");
+  }
+  console.log("listening port ${PORT}");
+});
