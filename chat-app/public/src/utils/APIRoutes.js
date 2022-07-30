b68d552014d02b970f1c41271ed6d2568ac99ff4
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-07-30 10:46:57 +0530

    tesla server

--- chat-app/public/src/utils/APIRoutes.js
+++ chat-app/public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5090";
+export const host = "https://apitesla.herokuapp.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
